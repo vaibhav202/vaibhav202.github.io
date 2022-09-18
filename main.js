63b078914e1caec6c2479c29d64fc0bf6f472100
Vaibhav Pancholi <71831734+vaibhavpancholi05@users.noreply.github.com>
2022-09-19 00:00:52 +0530

    Update

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 var themeToggler = document.querySelector('.theme-toggle'); 
-
 themeToggler.onclick = function theme_toggler(){
     document.body.classList.toggle("dark");
-
     if(document.body.classList.contains("dark")){
         document.querySelector('.theme-state').innerHTML = "Switch to light mode";
     }
     else{
         document.querySelector('.theme-state').innerHTML = "Switch to dark mode";  
     }
-}    
+}
